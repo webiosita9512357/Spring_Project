64a15fa32485218792eefa38e18de0d6ecb5ee3e
= <viktor.hajdu@brainit.sk>
2023-03-04 22:48:10 +0100

    logger + lombok dependecy added

--- src/main/java/com/example/demo/controllers/AccountController.java
+++ src/main/java/com/example/demo/controllers/AccountController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.entity.Account;
 import com.example.demo.service.AccountService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,8 @@ import java.util.List;
 
 @RestController
 public class AccountController {
+
+    private final Logger logger = org.slf4j.LoggerFactory.getLogger(AccountController.class);
 
     @Autowired
     private AccountService accountService;
@@ -30,16 +33,19 @@ public class AccountController {
     }
     @GetMapping("/account/{id}")
     public Account getAccount(@PathVariable("id") Long id) {
+        logger.info("In getAccount - AccountController");
         return accountService.getAccount(id);
     }
 
     @GetMapping("/accounts")
     public List<Account> getAccounts() {
+        logger.info("In getAccounts - AccountController");
         return accountService.getAccounts();
     }
 
     @GetMapping("/account/searchUsers/{name}")
     public List<Account> searchUsers(@PathVariable("name") String name) {
+        logger.info("In searchUsers - Searching for: " + name + " - AccountController");
         return accountService.searchByNameAccounts(name);
     }
 
--- src/main/java/com/example/demo/entity/Account.java
+++ src/main/java/com/example/demo/entity/Account.java
@@ -6,9 +6,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Account {
 
     // attributes
@@ -24,69 +32,5 @@ public class Account {
     @Length(min = 5, message = "Username must be at least 5 characters long")
     private String userName;
 
-    // constructors
-    public Account(Long id, String name, String lastName, String email, String userName) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.email = email;
-        this.userName = userName;
-    }
 
-    // default constructor
-    public Account() {
-    }
-
-    // toString
-    @Override
-    public String toString() {
-        return "Account{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", username='" + userName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-
-    // getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
